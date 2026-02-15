449df1ef8685894e81e88191772bf07b5cea4252
Gavin Juarez <2022157044@.edu.bz>
2026-02-14 19:05:55 -0600

    Complete Exercise 3: Counter App

---
+++ exercise3.js
@@ -0,0 +1,16 @@
+const display = document.querySelector('#count-display');
+
+const CounterApp = {
+  count: 0,
+  increment: function() {
+    this.count++;
+    display.textContent = this.count;
+  },
+  decrement: function() {
+    this.count--;
+    display.textContent = this.count;
+  }
+};
+
+document.querySelector('#inc-btn').addEventListener('click', CounterApp.increment.bind(CounterApp));
+document.querySelector('#dec-btn').addEventListener('click', CounterApp.decrement.bind(CounterApp));
